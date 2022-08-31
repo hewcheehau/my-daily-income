895e0ecfb952a2a457ec7b1346b6e8b286e1dc69
hewcheehau <=>
2022-08-31 20:03:13 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+// ./src/index.js
+// importing the dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+console.log(error);
+});
+
+database.once('connected', () => {
+console.log('Database connected');
+});
+
+//defining the Express app
+const app = express();
+//defining the array to work as the database (temporary solution)
+const ads = [{title: 'hello, hew (again)'}];
+
+//adding Helmet to enhance your REST Api's security
+app.use(helmet());
+
+//using bodyParser to parse JSON bodies into JS objets
+//app.use(bodyParser.json());
+app.use(express.json())
+//enabling CORS for all requests
+app.use(cors());
+
+//addding morgan to log HTTP requests 
+app.use(morgan('combined'));
+app.use(
+	express.urlencoded({
+	  extended: false,
+	})
+  );
+//use Routes file
+app.use('/v1', routes);
+//defining an endpoint to return all ads
+app.get('/', (req, res) => {
+	res.send(ads);
+});
+
+
+
+//starting the server
+app.listen(3001,() =>{console.log('listening 300');});
---
+++ src/routes/routes.js
@@ -0,0 +1,73 @@
+//routes file
+
+const express = require('express');
+
+const router = express.Router();
+
+module.exports = router;
+const Model = require('../models/model');
+//Post method
+router.post('/post', async (req, res) => {
+    // console.log("hellos");
+    // console.dir(req.body);
+    const data = new Model({name: req.body.name, age: req.body.age});
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    //
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+//GET method
+router.get('/getAll', async (req, res) => {
+	try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+});
+
+//GET by ID
+router.get('/getOne/:id', async(req, res) => {
+    try{
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+});
+//UPDATE by ID
+router.patch('/update/:id',async (req, res) => {
+	try{
+	const id = req.params.id;
+	const updatedData = req.body;
+	const options = {new: true};
+	const data = await Model.findByIdAndUpdate(
+		id, updatedData, options
+	);
+	res.send(data);
+	}catch (err) {
+	 res.status(500).json({message: err})
+	}
+});
+//DELETE by ID
+//Delete by ID Method
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+
